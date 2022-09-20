dce74018474b5b3059e9ea4e8d4588d1bd7629b8
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-09-20 11:05:29 +0430

    Test Employee Service Test with spy test double

--- test/AspNetCore.Test.Unit/Empoyee/EmployeeServiceTest.cs
+++ test/AspNetCore.Test.Unit/Empoyee/EmployeeServiceTest.cs
@@ -1,5 +1,7 @@
-﻿using ConsoleApp.Employees;
+﻿using AspNetCore.Test.Unit.Empoyee.TestDoubles;
+using ConsoleApp.Employees;
 using ConsoleApp.Infrastructure.Repository;
+using FluentAssertions;
 using NSubstitute;
 
 namespace AspNetCore.Test.Unit.Empoyee
@@ -16,6 +18,20 @@ namespace AspNetCore.Test.Unit.Empoyee
             employeeService.RegisterEmployee(new EmployeeDto { Id= 1, Firstname = "john", Lastname = "doe" });
 
             mockRepository.Received(1).Create(Arg.Is<Employee>(e => e.Firstname == "john" && e.Lastname == "doe"));
+        }
+
+        [Fact]
+        public void save_employee_on_registration_with_spy()
+        {
+            var repository = new EmployeeRepositorySpy();
+            var service = new EmployeeService(repository);
+
+            service.RegisterEmployee(new EmployeeDto { Id = 1, Firstname = "john", Lastname = "doe" });
+
+            repository.GetNumberOfCreateEmployees.Should().Be(1);
+            repository.GetByFirstItem().Id.Should().Be(1);
+            repository.GetByFirstItem().Firstname.Should().Be("john");
+            repository.GetByFirstItem().Lastname.Should().Be("doe");
         }
     }
 }
---
+++ test/AspNetCore.Test.Unit/Empoyee/TestDoubles/EmployeeRepositorySpy.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp.Employees;
+using ConsoleApp.Infrastructure.Repository;
+
+namespace AspNetCore.Test.Unit.Empoyee.TestDoubles
+{
+    public class EmployeeRepositorySpy : IEmployeeRepository
+    {
+        private Employee Employee;
+        private int CallNumbers;
+
+        public void Create(Employee employee)
+        {
+            this.Employee = employee;
+            CallNumbers++;
+        }
+
+        public Employee GetByFirstItem() => this.Employee;
+
+        public int GetNumberOfCreateEmployees => CallNumbers;
+    }
+}
