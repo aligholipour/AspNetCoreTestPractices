cc0a471734161dc9e18c1787601fe05b607fc95d
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-09-20 11:16:45 +0430

    Test EmplyoeeServiceTestSelfShunt with spy test double and 'Self Shunt' technique

---
+++ test/AspNetCore.Test.Unit/Empoyee/EmplyoeeServiceTestSelfShunt.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp.Employees;
+using ConsoleApp.Infrastructure.Repository;
+using FluentAssertions;
+
+namespace AspNetCore.Test.Unit.Empoyee
+{
+    public class EmplyoeeServiceTestSelfShunt : IEmployeeRepository
+    {
+        private Employee CreateEmployee;
+        private int NumberOfCreateEmployee;
+
+        [Fact]
+        public void save_employee_on_registration_with_spy()
+        {
+            var service = new EmployeeService(this);
+
+            service.RegisterEmployee(new EmployeeDto { Id = 1, Firstname = "john", Lastname = "doe" });
+
+            NumberOfCreateEmployee.Should().Be(1);
+
+            CreateEmployee.Id.Should().Be(1);
+            CreateEmployee.Firstname.Should().Be("john");
+            CreateEmployee.Lastname.Should().Be("doe");
+        }
+
+        public void Create(Employee employee)
+        {
+            this.CreateEmployee = employee;
+            NumberOfCreateEmployee++;
+        }
+
+        public Employee GetByFirstItem()
+        {
+            return this.CreateEmployee;
+        }
+    }
+}
