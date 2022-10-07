fedac98fbce14a827f7faf350e44bd47b945e34c
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-10-07 22:35:34 +0330

    Clearing additional namespaces

--- test/AspNetCore.Test.Integration/Utils/Constants.cs
+++ test/AspNetCore.Test.Integration/Utils/Constants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AspNetCore.Test.Integration.Utils
+﻿namespace AspNetCore.Test.Integration.Utils
 {
     public class Constants
     {
