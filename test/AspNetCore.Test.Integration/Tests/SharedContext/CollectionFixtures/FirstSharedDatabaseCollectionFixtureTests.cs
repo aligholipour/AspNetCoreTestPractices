898fc5646ab3de6f36450de4fd6c35d4a6c11229
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-11-02 00:09:19 +0330

    Add FirstSharedDatabaseCollectionFixtureTests

---
+++ test/AspNetCore.Test.Integration/Tests/SharedContext/CollectionFixtures/FirstSharedDatabaseCollectionFixtureTests.cs
@@ -0,0 +1,24 @@
+﻿namespace AspNetCore.Test.Integration.Tests.SharedContext.CollectionFixtures
+{
+    [Collection("CollectionFixture")]
+    public class FirstSharedDatabaseCollectionFixtureTests
+    {
+        private readonly DatabaseFixture _fixture;
+        public FirstSharedDatabaseCollectionFixtureTests(DatabaseFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var randomGuid = _fixture.RandomId; // randomGuid = f1c74b4a-6e89-4525-9c42-7eca302458a9;
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var randomGuid = _fixture.RandomId; // randomGuid = f1c74b4a-6e89-4525-9c42-7eca302458a9;
+        }
+    }
+}
