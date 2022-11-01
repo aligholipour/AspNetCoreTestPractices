dc94533e47fc3a75afaef0cb25448c919ee91a5f
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-11-01 23:24:55 +0330

    Add DatabaseFixture for share context

---
+++ test/AspNetCore.Test.Integration/Tests/SharedContext/DatabaseFixture.cs
@@ -0,0 +1,15 @@
+﻿namespace AspNetCore.Test.Integration.Tests.SharedContext
+{
+    public class DatabaseFixture : IDisposable
+    {
+        public Guid RandomId { get; private set; }
+        public DatabaseFixture()
+        {
+            RandomId = Guid.NewGuid();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
