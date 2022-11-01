a54cc145522d77a0aff448f44931fb5b84e6f719
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-11-02 00:12:13 +0330

    Add SecondSharedDatabaseCollectionFixtureTests

--- test/AspNetCore.Test.Integration/Tests/SharedContext/CollectionFixtures/FirstSharedDatabaseCollectionFixtureTests.cs
+++ test/AspNetCore.Test.Integration/Tests/SharedContext/CollectionFixtures/FirstSharedDatabaseCollectionFixtureTests.cs
@@ -21,4 +21,20 @@
             var randomGuid = _fixture.RandomId; // randomGuid = f1c74b4a-6e89-4525-9c42-7eca302458a9;
         }
     }
+
+    [Collection("CollectionFixture")]
+    public class SecondSharedDatabaseCollectionFixtureTests
+    {
+        private readonly DatabaseFixture _fixture;
+        public SecondSharedDatabaseCollectionFixtureTests(DatabaseFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var randomGuid = _fixture.RandomId; // randomGuid = f1c74b4a-6e89-4525-9c42-7eca302458a9;
+        }
+    }
 }
