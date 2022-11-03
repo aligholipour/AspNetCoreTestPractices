12b9a1fc871e659724087bafd141f616b23fba2e
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-11-03 23:17:41 +0330

    Add TaxCalculateValues for use MemberData instead InlineData

--- test/AspNetCore.Test.Unit/TaxCalculate/TaxtCalculatorTest.cs
+++ test/AspNetCore.Test.Unit/TaxCalculate/TaxtCalculatorTest.cs
@@ -9,6 +9,7 @@ namespace AspNetCore.Test.Unit.TaxCalculate
         [InlineData(1000000, 9, 910000)]
         [InlineData(1000000, 9.5, 905000)]
         [InlineData(1000000, 0, 1000000)]
+        //[MemberData(nameof(TaxCalculateValues))] Using MemeberData
         [Theory]
         public void tax_is_subtrack_from_salary(long salary, double taxRate, double expected)
         {
@@ -18,6 +19,19 @@ namespace AspNetCore.Test.Unit.TaxCalculate
             var salaryWithoutTax = service.CalculateSalary(salary);
 
             salaryWithoutTax.Should().Be(expected);
+        }
+
+        public static IEnumerable<object[]> TaxCalculateValues
+        {
+            get
+            {
+                return new List<object[]>
+                {
+                    new object[] { 1000000, 9, 910000 },
+                    new object[] { 1000000, 9.5, 905000 },
+                    new object[] { 1000000, 0, 1000000 }
+                };
+            }
         }
     }
 }
